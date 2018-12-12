bb24ec03d16f40fe1d442f9a08ea7729657e094e
Timothée Boissière <timothee.boissiere-junot@insa-lyon.fr>
2018-12-12 14:21:47 +0100

    V1 filtrage de sport

--- src/Controller/VisualisationCdsController.php
+++ src/Controller/VisualisationCdsController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Performance;
 use App\Entity\Sport;
 use App\Entity\Jointuresport;
+use App\Form\FiltresportType;
 
 class VisualisationCdsController extends AbstractController
 {   
@@ -21,13 +22,50 @@ class VisualisationCdsController extends AbstractController
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
         $this->denyAccessUnlessGranted('ROLE_Admin');
         $allPerformance = $this->getDoctrine()->getRepository(Performance::class)->findAll(); //->findByPerFkutilisateur($utilisateur);
-            
+
             return $this->render('/visualisation_cds/index.html.twig', array(
                 'allPerf' => $allPerformance,
-                'sport' => $sport,
                 'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ));
     }   
+
+    /**
+     * @Route("/visualisation/performance/cds/sport", name="visualiserPerformanceCdsSport")
+     */
+
+    public function visualiserPerformanceCdsSport(Request $request)
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $this->denyAccessUnlessGranted('ROLE_Admin');
+        $sport = array();
+        $form = $this->createForm(FiltresportType::class, $sport);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            return $this->redirectToRoute('visualiserPerformanceCdsSportId',array('idSport' => $form->get('spoId')->getData()->getSpoId()));
+        }
+
+        return $this->render('/visualisation_cds/filtresport.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/visualisation/performance/cds/sport/{idSport}", name="visualiserPerformanceCdsSportId")
+     */
+
+    public function visualiserPerformanceCdsSportId(Request $request, $idSport)
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $this->denyAccessUnlessGranted('ROLE_Admin');
+        $allJointure = $this->getDoctrine()->getRepository(Jointuresport::class)->findByJoispoFksport($idSport); //->findByPerFkutilisateur($utilisateur);
+        $allPerformance = $this->getDoctrine()->getRepository(Performance::class)->findByPerFkjointuresport($allJointure);
+
+            return $this->render('/visualisation_cds/index.html.twig', array(
+                'allPerf' => $allPerformance,
+                'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ));
+    }
 
     /**
      * @Route("/visualisation/profil/cds", name="visualiserProfilCds")
